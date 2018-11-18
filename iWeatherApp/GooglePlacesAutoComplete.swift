da8d48e02b2a75333d5937eddb41d5ea8bf20be1
dinoomanovic <dino.omanovic.15@gmail.com>
2018-11-18 09:26:45 +0100

    initial work

---
+++ iWeatherApp/GooglePlacesAutoComplete.swift
@@ -0,0 +1,406 @@
+//
+//  GooglePlacesAutoComplete.swift
+//  iWeatherApp
+//
+//  Created by Dino  Omanovic on 24/09/2017.
+//  Copyright © 2017 Dino  Omanovic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public let ErrorDomain: String! = "GooglePlacesAutocompleteErrorDomain"
+
+public struct LocationBias {
+    public let latitude: Double
+    public let longitude: Double
+    public let radius: Int
+    
+    public init(latitude: Double = 0, longitude: Double = 0, radius: Int = 20000000) {
+        self.latitude = latitude
+        self.longitude = longitude
+        self.radius = radius
+    }
+    
+    public var location: String {
+        return "\(latitude),\(longitude)"
+    }
+}
+
+public enum PlaceType: CustomStringConvertible {
+    case All
+    case Geocode
+    case Address
+    case Establishment
+    case Regions
+    case Cities
+    
+    public var description : String {
+        switch self {
+        case .All: return ""
+        case .Geocode: return "geocode"
+        case .Address: return "address"
+        case .Establishment: return "establishment"
+        case .Regions: return "(regions)"
+        case .Cities: return "(cities)"
+        }
+    }
+}
+
+public class Place: NSObject {
+    public let id: String
+    public let desc: String
+    public var apiKey: String?
+    
+    override public var description: String {
+        get { return desc }
+    }
+    
+    public init(id: String, description: String) {
+        self.id = id
+        self.desc = description
+    }
+    
+    public convenience init(prediction: [String: AnyObject], apiKey: String?) {
+        self.init(
+            id: prediction["place_id"] as! String,
+            description: prediction["description"] as! String
+        )
+        
+        self.apiKey = apiKey
+    }
+    
+    /**
+     Call Google Place Details API to get detailed information for this place
+     
+     Requires that Place#apiKey be set
+     
+     - parameter result: Callback on successful completion with detailed place information
+     */
+    public func getDetails(result: @escaping (PlaceDetails) -> ()) {
+        GooglePlaceDetailsRequest(place: self).request(result: result)
+    }
+}
+
+public class PlaceDetails: CustomStringConvertible {
+    public let name: String
+    public let latitude: Double
+    public let longitude: Double
+    public let raw: [String: AnyObject]
+    
+    public init(json: [String: AnyObject]) {
+        let result = json["result"] as! [String: AnyObject]
+        let geometry = result["geometry"] as! [String: AnyObject]
+        let location = geometry["location"] as! [String: AnyObject]
+        
+        self.name = result["name"] as! String
+        self.latitude = location["lat"] as! Double
+        self.longitude = location["lng"] as! Double
+        self.raw = json
+    }
+    
+    public var description: String {
+        return "PlaceDetails: \(name) (\(latitude), \(longitude))"
+    }
+}
+
+@objc public protocol GooglePlacesAutocompleteDelegate {
+    @objc optional func placesFound(places: [Place])
+    @objc optional func placeSelected(place: Place)
+    @objc optional func placeViewClosed()
+}
+
+// MARK: - GooglePlacesAutocomplete
+public class GooglePlacesAutocomplete: UINavigationController {
+    public var gpaViewController: GooglePlacesAutocompleteContainer!
+    public var closeButton: UIBarButtonItem!
+    
+    // Proxy access to container navigationItem
+    public override var navigationItem: UINavigationItem {
+        get { return gpaViewController.navigationItem }
+    }
+    
+    public var placeDelegate: GooglePlacesAutocompleteDelegate? {
+        get { return gpaViewController.delegate }
+        set { gpaViewController.delegate = newValue }
+    }
+    
+    public var locationBias: LocationBias? {
+        get { return gpaViewController.locationBias }
+        set { gpaViewController.locationBias = newValue }
+    }
+    
+    public convenience init(apiKey: String, placeType: PlaceType = .All) {
+        let gpaViewController = GooglePlacesAutocompleteContainer(
+            apiKey: apiKey,
+            placeType: placeType
+        )
+        
+        self.init(rootViewController: gpaViewController)
+        self.gpaViewController = gpaViewController
+        
+        closeButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.stop, target: self, action: #selector(GooglePlacesAutocomplete.close))
+        closeButton.style = UIBarButtonItemStyle.done
+        
+        gpaViewController.navigationItem.leftBarButtonItem = closeButton
+        gpaViewController.navigationItem.title = "Enter Address"
+    }
+    
+    func close() {
+        placeDelegate?.placeViewClosed?()
+    }
+    
+    public func reset() {
+        gpaViewController.searchBar.text = ""
+        gpaViewController.searchBar(gpaViewController.searchBar, textDidChange: "")
+    }
+}
+
+// MARK: - GooglePlacesAutocompleteContainer
+public class GooglePlacesAutocompleteContainer: UIViewController {
+    @IBOutlet public weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var topConstraint: NSLayoutConstraint!
+    
+    var delegate: GooglePlacesAutocompleteDelegate?
+    var apiKey: String?
+    var places = [Place]()
+    var placeType: PlaceType = .All
+    var locationBias: LocationBias?
+    
+    convenience init(apiKey: String, placeType: PlaceType = .All) {
+        let bundle = Bundle(for: GooglePlacesAutocompleteContainer.self)
+        
+        self.init(nibName: "GooglePlacesAutocomplete", bundle: bundle)
+        self.apiKey = apiKey
+        self.placeType = placeType
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override public func viewWillLayoutSubviews() {
+        topConstraint.constant = topLayoutGuide.length
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: Selector(("keyboardWasShown:")), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: Selector(("keyboardWillBeHidden:")), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        searchBar.becomeFirstResponder()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    func keyboardWasShown(notification: NSNotification) {
+        if isViewLoaded && view.window != nil {
+            let info: Dictionary = notification.userInfo!
+            let keyboardSize: CGSize = ((info[UIKeyboardFrameBeginUserInfoKey] as AnyObject).cgRectValue.size)
+            let contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize.height, 0.0)
+            
+            tableView.contentInset = contentInsets;
+            tableView.scrollIndicatorInsets = contentInsets;
+        }
+    }
+    
+    func keyboardWillBeHidden(notification: NSNotification) {
+        if isViewLoaded && view.window != nil {
+            self.tableView.contentInset = UIEdgeInsets.zero
+            self.tableView.scrollIndicatorInsets = UIEdgeInsets.zero
+        }
+    }
+}
+
+// MARK: - GooglePlacesAutocompleteContainer (UITableViewDataSource / UITableViewDelegate)
+extension GooglePlacesAutocompleteContainer: UITableViewDataSource, UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
+    }
+    
+    @nonobjc public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // Get the corresponding candy from our candies array
+        let place = self.places[indexPath.row]
+        
+        // Configure the cell
+        cell.textLabel!.text = place.description
+        cell.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+        
+        return cell
+    }
+    
+    public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: IndexPath) {
+        delegate?.placeSelected?(place: self.places[indexPath.row])
+    }
+}
+
+// MARK: - GooglePlacesAutocompleteContainer (UISearchBarDelegate)
+extension GooglePlacesAutocompleteContainer: UISearchBarDelegate {
+    public func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        if (searchText == "") {
+            self.places = []
+            tableView.isHidden = true
+        } else {
+            getPlaces(searchString: searchText)
+        }
+    }
+    
+    /**
+     Call the Google Places API and update the view with results.
+     - parameter searchString: The search query
+     */
+    
+    private func getPlaces(searchString: String) {
+        var params = [
+            "input": searchString,
+            "types": placeType.description,
+            "key": apiKey ?? ""
+        ]
+        
+        if let bias = locationBias {
+            params["location"] = bias.location
+            params["radius"] = bias.radius.description
+        }
+        
+        if (searchString == ""){
+            return
+        }
+        
+        GooglePlacesRequestHelpers.doRequest(
+            url: "https://maps.googleapis.com/maps/api/place/autocomplete/json",
+            params: params
+        ) { json, error in
+            if let json = json{
+                if let predictions = json["predictions"] as? Array<[String: AnyObject]> {
+                    self.places = predictions.map { (prediction: [String: AnyObject]) -> Place in
+                        return Place(prediction: prediction, apiKey: self.apiKey)
+                    }
+                    self.tableView.reloadData()
+                    self.tableView.isHidden = false
+                    self.delegate?.placesFound?(places: self.places)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - GooglePlaceDetailsRequest
+class GooglePlaceDetailsRequest {
+    let place: Place
+    
+    init(place: Place) {
+        self.place = place
+    }
+    
+    func request(result: @escaping (PlaceDetails) -> ()) {
+        GooglePlacesRequestHelpers.doRequest(
+            url: "https://maps.googleapis.com/maps/api/place/details/json",
+            params: [
+                "placeid": place.id,
+                "key": place.apiKey ?? ""
+            ]
+        ) { json, error in
+            if let json = json as? [String: AnyObject] {
+                result(PlaceDetails(json: json))
+            }
+            if let error = error {
+                // TODO: We should probably pass back details of the error
+                print("Error fetching google place details: \(error)")
+            }
+        }
+    }
+}
+
+// MARK: - GooglePlacesRequestHelpers
+class GooglePlacesRequestHelpers {
+    /**
+     Build a query string from a dictionary
+     - parameter parameters: Dictionary of query string parameters
+     - returns: The properly escaped query string
+     */
+    private class func query(parameters: [String: AnyObject]) -> String {
+        var components: [(String, String)] = []
+        for key in Array(parameters.keys).sorted(by: <) {
+            let value: AnyObject! = parameters[key]
+            components += [(escape(string: key), escape(string: "\(value)"))]
+        }
+        
+        return (components.map{"\($0)=\($1)"} as [String]).joined(separator: "&")
+    }
+    
+    private class func escape(string: String) -> String {
+        let legalURLCharactersToBeEscaped: CFString = ":/?&=;+!@#$()',*" as CFString
+        return CFURLCreateStringByAddingPercentEscapes(nil, string as CFString, nil, legalURLCharactersToBeEscaped, CFStringBuiltInEncodings.UTF8.rawValue) as String
+    }
+    
+     public class func doRequest(url: String, params: [String: String], completion: @escaping (NSDictionary?,NSError?) -> ()) {
+        let request = NSMutableURLRequest(
+            url: NSURL(string: "\(url)?\(query(parameters: params as [String : AnyObject]))")! as URL
+        )
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            self.handleResponse(data: data! as NSData, response: response as? HTTPURLResponse, error: error! as NSError, completion: completion)
+        }
+        
+        task.resume()
+    }
+    
+    private class func handleResponse(data: NSData!, response: HTTPURLResponse!, error: NSError!, completion: @escaping (NSDictionary?, NSError?) -> ()) {
+        
+        // Always return on the main thread...
+        let done: ((NSDictionary?, NSError?) -> Void) = {(json, error) in
+            DispatchQueue.main.async(execute: {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                completion(json,error)
+            })
+        }
+        
+        if let error = error {
+            print("GooglePlaces Error: \(error.localizedDescription)")
+            done(nil,error)
+            return
+        }
+        
+        if response == nil {
+            print("GooglePlaces Error: No response from API")
+            let error = NSError(domain: ErrorDomain, code: 1001, userInfo: [NSLocalizedDescriptionKey:"No response from API"])
+            done(nil,error)
+            return
+        }
+        
+        if response.statusCode != 200 {
+            print("GooglePlaces Error: Invalid status code \(response.statusCode) from API")
+            let error = NSError(domain: ErrorDomain, code: response.statusCode, userInfo: [NSLocalizedDescriptionKey:"Invalid status code"])
+            done(nil,error)
+            return
+        }
+        
+        let json: NSDictionary?
+        do {
+            json = try JSONSerialization.jsonObject(
+                with: data as Data,
+                options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+        } catch {
+            print("Serialisation error")
+            let serialisationError = NSError(domain: ErrorDomain, code: 1002, userInfo: [NSLocalizedDescriptionKey:"Serialization error"])
+            done(nil,serialisationError)
+            return
+        }
+        
+        if let status = json?["status"] as? String {
+            if status != "OK" {
+                print("GooglePlaces API Error: \(status)")
+                let error = NSError(domain: ErrorDomain, code: 1002, userInfo: [NSLocalizedDescriptionKey:status])
+                done(nil,error)
+                return
+            }
+        }
+        
+        done(json,nil)
+        
+    }
+}
---
+++ iWeatherApp/ViewController.swift
@@ -0,0 +1,213 @@
+//
+//  ViewController.swift
+//  iWeatherApp
+//
+//  Created by Dino  Omanovic on 16/09/2017.
+//  Copyright © 2017 Dino  Omanovic. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+import SwiftyJSON
+import GooglePlaces
+
+class ViewController: UIViewController {
+    @IBOutlet weak var titleText: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherDescription: UILabel!
+    @IBOutlet weak var currentTemperature: UILabel!
+    @IBOutlet weak var dayOne: UILabel!
+    @IBOutlet weak var dayTwo: UILabel!
+    @IBOutlet weak var dayThree: UILabel!
+    @IBOutlet weak var imageOne: UIImageView!
+    @IBOutlet weak var imageTwo: UIImageView!
+    @IBOutlet weak var imageThree: UIImageView!
+    @IBOutlet weak var temperatureOne: UILabel!
+    @IBOutlet weak var temperatureTwo: UILabel!
+    @IBOutlet weak var temperatureThree: UILabel!
+    @IBAction func autocompleteClicked(_ sender: UIButton) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    private let APPID = "14c1ad343b75d7f6fbe4f14fd766f0f7"
+    private let unit = "metric"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GMSPlacesClient.provideAPIKey("AIzaSyAEcRoM-h3AXb5MxaEwMNGvpyTfQpU-xN0")
+        getWeather(cityName: "Tuzla,BA")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getWeather(cityName: String) {
+        Alamofire.request("https://api.openweathermap.org/data/2.5/forecast", parameters: ["q":cityName,"APPID":APPID,"units":unit])
+            .responseJSON {
+                response in
+                if let result = response.result.value {
+                    let json = JSON(result)
+                    print(json)
+                    let cityName = String(describing: json["city"]["name"])
+                    self.titleText.text = cityName
+                    let currentImage = String(describing:json["list"][0]["weather"][0]["icon"])
+                    let dayOneImage = String(describing:json["list"][8]["weather"][0]["icon"])
+                    let dayTwoImage = String(describing:json["list"][16]["weather"][0]["icon"])
+                    let dayThreeImage = String(describing:json["list"][24]["weather"][0]["icon"])
+                    print(currentImage)
+                    self.currentWeatherImage.image = UIImage(named: (self.getIcon(iconName: currentImage)))
+                    self.imageOne.image = UIImage(named: (self.getIcon(iconName: dayOneImage)))
+                    self.imageTwo.image = UIImage(named: (self.getIcon(iconName: dayTwoImage)))
+                    self.imageThree.image = UIImage(named: (self.getIcon(iconName: dayThreeImage)))
+                    let currentDesc = String(describing:json["list"][0]["weather"][0]["description"])
+                    let dayOneDesc = String(describing:json["list"][8]["weather"][0]["description"])
+                    let dayTwoDesc = String(describing:json["list"][16]["weather"][0]["description"])
+                    let dayThreeDesc = String(describing:json["list"][24]["weather"][0]["description"])
+                    let currentTempFloat = String(describing: json["list"][0]["main"]["temp"])
+                    let dayOneValue = String(describing: json["list"][8]["main"]["temp"])
+                    let dayTwoValue = String(describing: json["list"][16]["main"]["temp"])
+                    let dayThreeValue = String(describing: json["list"][24]["main"]["temp"])
+                    let currentTempInt = (currentTempFloat as NSString).integerValue
+                    let dayOneInt = (dayOneValue as NSString).integerValue
+                    let dayTwoInt = (dayTwoValue as NSString).integerValue
+                    let dayThreeInt = (dayThreeValue as NSString).integerValue
+                    let currentTemp = String(describing: currentTempInt)
+                    let dayOneTempVal = String(describing: dayOneInt)
+                    let dayTwoTempVal = String(describing: dayTwoInt)
+                    let dayThreeTempVal = String(describing: dayThreeInt)
+                    self.currentTemperature.text = currentTemp + " °C"
+                    self.imageOne.image = UIImage(named: (self.getIcon(iconName:dayOneImage)))
+                    self.temperatureOne.text = dayOneTempVal + " °C"
+                    self.imageTwo.image = UIImage(named: (self.getIcon(iconName:dayTwoImage)))
+                    self.temperatureTwo.text = dayTwoTempVal + " °C"
+                    self.imageThree.image = UIImage(named: (self.getIcon(iconName:dayThreeImage)))
+                    self.temperatureThree.text = dayThreeTempVal + " °C"
+                    self.dayOne.text = dayOneDesc
+                    self.dayTwo.text = dayTwoDesc
+                    self.dayThree.text = dayThreeDesc
+                    self.currentWeatherDescription.text = currentDesc
+                    
+                }
+        }
+    }
+    
+    func getIcon(iconName: String)->String {
+        switch(iconName) {
+        case "01d":
+            return "d01"
+        case "01n":
+            return "n01"
+        case "02d":
+            return "d02"
+        case "02n":
+            return "n02"
+        case "03d":
+            return "d03"
+        case "03n":
+            return "n03"
+        case "04d":
+            return "d04"
+        case "04n":
+            return "n04"
+        case "09d":
+            return "d09"
+        case "09n":
+            return "n09"
+        case "10d":
+            return "d10"
+        case "10n":
+            return "n10"
+        case "11d":
+            return "d11"
+        case "11n":
+            return "n11"
+        case "13d":
+            return "d13"
+        case "13n":
+            return "n13"
+        default:
+            return "n01"
+        }
+    }
+    
+}
+
+struct ReversedGeoLocation {
+    let name: String            // eg. Apple Inc.
+    let streetName: String      // eg. Infinite Loop
+    let streetNumber: String    // eg. 1
+    let city: String            // eg. Cupertino
+    let state: String           // eg. CA
+    let zipCode: String         // eg. 95014
+    let country: String         // eg. United States
+    let isoCountryCode: String  // eg. US
+    
+    var formattedAddress: String {
+        return """
+        \(name),
+        \(streetNumber) \(streetName),
+        \(city), \(state) \(zipCode)
+        \(country)
+        """
+    }
+    
+    // Handle optionals as needed
+    init(with placemark: CLPlacemark) {
+        self.name           = placemark.name ?? ""
+        self.streetName     = placemark.thoroughfare ?? ""
+        self.streetNumber   = placemark.subThoroughfare ?? ""
+        self.city           = placemark.locality ?? ""
+        self.state          = placemark.administrativeArea ?? ""
+        self.zipCode        = placemark.postalCode ?? ""
+        self.country        = placemark.country ?? ""
+        self.isoCountryCode = placemark.isoCountryCode ?? ""
+    }
+}
+
+extension ViewController: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        let location = CLLocation(latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            
+            guard let placemark = placemarks?.first else {
+                let errorString = error?.localizedDescription ?? "Unexpected Error"
+                print("Unable to reverse geocode the given location. Error: \(errorString)")
+                return
+            }
+            
+            let reversedGeoLocation = ReversedGeoLocation(with: placemark)
+            print(reversedGeoLocation.isoCountryCode)
+            print(reversedGeoLocation.city)
+            self.getWeather(cityName: reversedGeoLocation.city+","+reversedGeoLocation.isoCountryCode)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+}
